5c014bb9e4d16bda1d4df66962f75ffd9e0129d5
Ahmed Elkady <ahmedelkady@Ahmeds-MacBook-Pro.local>
2026-06-27 12:24:53 +0300

    Initial Commit

---
+++ Shopify/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shopify
+//
+//  Created by Ahmed Elkady on 27/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shopify/ShopifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopifyApp.swift
+//  Shopify
+//
+//  Created by Ahmed Elkady on 27/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct ShopifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
