fb15855f7a07dc40592a9a2c1182689067308619
Mazen Amr <139044003+mazen675@users.noreply.github.com>
2026-06-30 20:44:02 +0300

    added fixes

--- Modules/product-details/Package.swift
+++ Modules/product-details/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.3
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -13,16 +13,21 @@ let package = Package(
             targets: ["ProductDetails"]
         ),
     ],
+    dependencies: [.package(path: "../shopify-network")]
+    ,
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ProductDetails"
+            name: "ProductDetails",
+            dependencies: [
+               // Explicitly tells SPM: "Look for the product 'ShopifyNetwork' inside the package 'shopify-network'"
+               .product(name: "ShopifyNetwork", package: "shopify-network")
+           ]
         ),
         .testTarget(
             name: "ProductDetailsTests",
-            dependencies: ["ProductDetails"]
+            dependencies: ["ProductDetails", .product(name: "ShopifyNetwork", package: "shopify-network")]
         ),
-    ],
-    swiftLanguageModes: [.v6]
+    ]
 )
