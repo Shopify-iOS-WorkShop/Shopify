b9c8f4cdac7291fe24ab696cb41569cf032ab1b7
Mazen Amr <139044003+mazen675@users.noreply.github.com>
2026-07-01 12:41:48 +0300

    integrated product-listing module into main application

--- Modules/product-listing/Package.swift
+++ Modules/product-listing/Package.swift
@@ -5,12 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "ProductListing",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v16)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ProductListing",
             targets: ["ProductListing"]),
+    ]
+    ,dependencies: [
+        .package(path: "../shopify-network")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -19,6 +22,6 @@ let package = Package(
             name: "ProductListing"),
         .testTarget(
             name: "ProductListingTests",
-            dependencies: ["ProductListing"]),
+            dependencies: ["ProductListing",.product(name: "ShopifyNetwork", package: "shopify-network")]),
     ]
 )
--- Shopify/ShopifyApp.swift
+++ Shopify/ShopifyApp.swift
@@ -16,10 +16,17 @@ struct ShopifyApp: App {
     var body: some Scene {
         WindowGroup {
             let client = URLSessionNetworkClient()
-            let repo = HomeRepository(networkClient: client)
-            let viewModel = HomeViewModel(repository: repo)
+            
+            let homeRepo = HomeRepository(networkClient: client)
+            let homeViewModel = HomeViewModel(repository: homeRepo)
+            let listingRepo = ProductListingRepository(networkClient: client)
+            //let testContext = ListingContext.collection(id: "550537363773", title: "ADIDAS")
+            let testContext = ListingContext.allProducts
+            
+            let listingViewModel = ProductListingViewModel(context: testContext, repository: listingRepo)
+            
             NavigationStack {
-                ProductListingView(title: "Women's Tops")
+                ProductListingView(title: "Women's Tops", viewModel: listingViewModel)
             }
         }
     }
